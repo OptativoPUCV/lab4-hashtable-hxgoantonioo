2f89f7ae71db92a37895f319afb028b3f68bf0aa
hxgoantonioo <163428772+hxgoantonioo@users.noreply.github.com>
2024-04-27 02:42:37 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -74,9 +74,20 @@ HashMap * createMap(long capacity)
 }
 
 void eraseMap(HashMap * map,  char * key) 
-{    
-
-
+{
+    if(map==NULL || key==NULL) return;
+    long posicion = hash(key,map->capacity);
+    while(map->buckets[posicion]!=NULL && map->buckets[posicion]->key!=NULL)
+      {
+        if(is_equal(map->buckets[posicion]->key,key))
+        {
+          map->buckets[posicion]=NULL;
+          map->size--;
+          return;
+        }
+        posicion++;
+        posicion%=map->capacity;
+      }
 }
 
 Pair * searchMap(HashMap * map,  char * key) 
