d35fe6f77676ab7f2c5d60cfcdd3563e77e9433c
hxgoantonioo <163428772+hxgoantonioo@users.noreply.github.com>
2024-04-27 02:53:19 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -124,17 +124,16 @@ Pair * firstMap(HashMap * map)
 
 Pair * nextMap(HashMap * map) 
 {
-    if(map==NULL) return NULL;
-    map->current++;
-    map->current%=map->capacity;
-    while(map->current!=-1)
+  if(map == NULL) return NULL;
+  long start = map->current;
+  map->current = (map->current + 1) % map->capacity;
+  while (map->current != start) 
+  {
+      if (map->buckets[map->current] != NULL && map->buckets[map->current]->key != NULL)
       {
-        if(map->buckets[map->current]!=NULL && map->buckets[map->current]->key!=NULL)
-        {
           return map->buckets[map->current];
-        }
-        map->current++;
-        map->current%=map->capacity;
       }
-    return NULL;
+      map->current = (map->current + 1) % map->capacity;
+  }
+  return NULL;
 }
